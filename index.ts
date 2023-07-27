fe11081c9a25b4258535268bda63c276e27ced40
thev3ritas <81315628+mikhailcodes@users.noreply.github.com>
2023-07-26 21:02:05 -0500

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,8 +1,29 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node'
 
+const fetchData = async () => {
+    try {
+        const response = await fetch(
+            "https://oku.club/api/users/profile/mikhailarden?format=json"
+        );
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error("Error fetching data:", error);
+        throw error;
+    }
+};
+
 export default function handler(req: VercelRequest, res: VercelResponse) {
-    const { name = 'World' } = req.query
-    return res.json({
-        message: `Hello ${name}!`,
-    })
+
+    const getData = async () => {
+        try {
+            const data = await fetchData();
+            return data;
+        } catch (error) {
+            return error;
+        }
+    };
+
+
+    return getData();
 }
